5b8b2fe055d2040779588fbc0fe053939c58010a
KhadijahJens <132610236+KhadijahJens@users.noreply.github.com>
2024-04-30 22:52:28 -0500

    updated with review changes

--- train_model.py
+++ train_model.py
@@ -75,15 +75,17 @@ print(f"Precision: {p:.4f} | Recall: {r:.4f} | F1: {fb:.4f}")
 
 # TODO: compute the performance on model slices using the performance_on_categorical_slice function
 # iterate through the categorical features
-for col in cat_features:
-    # iterate through the unique values in one categorical feature
-    for slicevalue in sorted(test[col].unique()):
-        count = test[test[col] == slicevalue].shape[0]
-        p, r, fb = performance_on_categorical_slice(
-            test, col, slicevalue, cat_features, "salary", encoder, lb, model
-            # use test, col and slicevalue as part of the input
-        )
-        output_file_path = "slice_output.txt"
-        with open("slice_output.txt", "a") as f:
+output_file_path = "slice_output.txt"
+with open(output_file_path, "a") as f:
+    # Iterate through the categorical features
+    for col in cat_features:
+        # Iterate through the unique values in one categorical feature
+        for slicevalue in sorted(train[col].unique()):
+            count = train[train[col] == slicevalue].shape[0]
+            # Compute performance metrics for the slice
+            p, r, fb = performance_on_categorical_slice(
+                train, col, slicevalue, cat_features, "salary", encoder, lb, model
+            )
+            # Write metrics to the output file
             print(f"{col}: {slicevalue}, Count: {count:,}", file=f)
             print(f"Precision: {p:.4f} | Recall: {r:.4f} | F1: {fb:.4f}", file=f)
